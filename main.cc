87f8f0d701ad1d3823de8c9b97aa254aaface68c
Przemek <przemyslaw.czechowski@gmail.com>
2018-11-27 20:02:03 +0100

    Separated active bodies. Does not compile

--- main.cc
+++ main.cc
@@ -4,6 +4,7 @@
 using state_type = std::vector<double>;
 
 int n_bodies = 1000;
+int n_active_bodies = 10;
 
 enum index_type {
 	pos_x = 0,
@@ -19,25 +20,30 @@ int idx(int body_number) {
 	return i*n_bodies + body_number;
 }
 
+int aidx(int body_number) {
+	return i*n_active_bodies + body_number;
+}
+
 
 void calculate_forces(const state_type &x, state_type &dxdt, const double t) {
-	state_type deltas(3*n_bodies);
-	state_type r(n_bodies);
-	for(int i = 0; i<n_bodies; ++i) {
+	std::vector<std::vector<double>> deltas(3*n_active_bodies, std::vector<double>(n_bodies));
+	std::vector<std::vector<double>> r(n_active_bodies, std::vector<double>(n_bodies));
+	for(int j = 0; j<n_bodies; ++j)
 		dxdt[idx<pos_x>(i)] = x[idx<pos_x>(i)];
 		dxdt[idx<pos_y>(i)] = x[idx<pos_y>(i)];
 		dxdt[idx<pos_z>(i)] = x[idx<pos_z>(i)];
+	for(int i = 0; i<n_active_bodies; ++i) {
 		for(int j = 0; j<n_bodies; ++j) {
-			deltas[idx<pos_x>(i)] = x[idx<pos_x>(j)] - x[idx<pos_x>(i)];
-			deltas[idx<pos_y>(i)] = x[idx<pos_y>(j)] - x[idx<pos_y>(i)];
-			deltas[idx<pos_z>(i)] = x[idx<pos_z>(j)] - x[idx<pos_z>(i)];
+			deltas[aidx<pos_x>(i)][j] = x[idx<pos_x>(j)] - x[idx<pos_x>(i)];
+			deltas[aidx<pos_y>(i)][j] = x[idx<pos_y>(j)] - x[idx<pos_y>(i)];
+			deltas[aidx<pos_z>(i)][j] = x[idx<pos_z>(j)] - x[idx<pos_z>(i)];
+			r[i][j]  = std::pow(deltas[aidx<pos_x>(i)][j], 2);
+			r[i][j] += std::pow(deltas[aidx<pos_y>(i)][j], 2);
+			r[i][j] += std::pow(deltas[aidx<pos_z>(i)][j], 2);
+			dxdt[idx<vel_x>(i)] += deltas[aidx<pos_x>(i)][j]/(r[i]*std::sqrt(r[i]));
+			dxdt[idx<vel_y>(i)] += deltas[aidx<pos_y>(i)][j]/(r[i]*std::sqrt(r[i]));
+			dxdt[idx<vel_z>(i)] += deltas[aidx<pos_z>(i)][j]/(r[i]*std::sqrt(r[i]));
 		}
-		r[i]  = std::pow(deltas[idx<pos_x>(i)], 2);
-		r[i] += std::pow(deltas[idx<pos_y>(i)], 2);
-		r[i] += std::pow(deltas[idx<pos_z>(i)], 2);
-		dxdt[idx<vel_x>(i)] = deltas[idx<pos_x>(i)]/(r[i]*std::sqrt(r[i]));
-		dxdt[idx<vel_y>(i)] = deltas[idx<pos_y>(i)]/(r[i]*std::sqrt(r[i]));
-		dxdt[idx<vel_z>(i)] = deltas[idx<pos_z>(i)]/(r[i]*std::sqrt(r[i]));
 	}
 }
 
