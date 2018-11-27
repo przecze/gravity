24e24fde75ccc882bc00b96ff26958fa160565a9
Przemek <przemyslaw.czechowski@gmail.com>
2018-11-27 19:44:05 +0100

    Naive force calculation

--- main.cc
+++ main.cc
@@ -1,8 +1,9 @@
 #include<iostream>
 #include<vector>
+#include<cmath>
 using state_type = std::vector<double>;
 
-int n_bodies = 10000;
+int n_bodies = 1000;
 
 enum index_type {
 	pos_x = 0,
@@ -20,21 +21,35 @@ int idx(int body_number) {
 
 
 void calculate_forces(const state_type &x, state_type &dxdt, const double t) {
-	auto deltas = new double[n_bodies*6];
+	state_type deltas(3*n_bodies);
+	state_type r(n_bodies);
 	for(int i = 0; i<n_bodies; ++i) {
 		dxdt[idx<pos_x>(i)] = x[idx<pos_x>(i)];
 		dxdt[idx<pos_y>(i)] = x[idx<pos_y>(i)];
 		dxdt[idx<pos_z>(i)] = x[idx<pos_z>(i)];
-		dxdt[idx<vel_x>(i)] = 0.;
-		dxdt[idx<vel_y>(i)] = 0.;
-		dxdt[idx<vel_z>(i)] = 0.;
+		for(int j = 0; j<n_bodies; ++j) {
+			deltas[idx<pos_x>(i)] = x[idx<pos_x>(j)] - x[idx<pos_x>(i)];
+			deltas[idx<pos_y>(i)] = x[idx<pos_y>(j)] - x[idx<pos_y>(i)];
+			deltas[idx<pos_z>(i)] = x[idx<pos_z>(j)] - x[idx<pos_z>(i)];
+			r[i]  = std::pow(deltas[idx<pos_x>(i)], 2);
+			r[i] += std::pow(deltas[idx<pos_y>(i)], 2);
+			r[i] += std::pow(deltas[idx<pos_z>(i)], 2);
+			dxdt[idx<vel_x>(i)] = deltas[idx<pos_x>(i)]/(r[i]*std::sqrt(r[i]));
+			dxdt[idx<vel_y>(i)] = deltas[idx<pos_y>(i)]/(r[i]*std::sqrt(r[i]));
+			dxdt[idx<vel_z>(i)] = deltas[idx<pos_z>(i)]/(r[i]*std::sqrt(r[i]));
+		}
 	}
 }
 
 int main() {
 	state_type a(6*n_bodies, 0.);
+	for(int i=0; i<n_bodies; ++i) {
+		a[idx<pos_x>(i)] = i;
+		a[idx<pos_y>(i)] = i;
+		a[idx<pos_z>(i)] = i;
+	}
 	state_type b(6*n_bodies);
-	for(int i=0; i<1e4; ++i) {
+	for(int i=0; i<1e2; ++i) {
 		calculate_forces(a, b, 1.);
 	}
 	//for(auto x: b) {
