b69c327b5221bcf621084460d406deecb803d360
Przemek <przemyslaw.czechowski@gmail.com>
2018-11-29 21:22:53 +0100

    Simple api for planets prediction

--- main.cc
+++ main.cc
@@ -1,6 +1,9 @@
-#include<vector>
+#include<valarray>
 #include<cmath>
 #include<array>
+#include<iostream>
+#include<functional>
+#include <boost/numeric/odeint.hpp>
 
 
 enum index_type {
@@ -12,48 +15,157 @@ enum index_type {
 	vel_z = 5,
 };
 
-using StateType = std::array<std::vector<double>, 6>;
+using ContainerType = std::array<std::valarray<double>, 3>;
+
+ContainerType get_container(size_t size) {
+	auto container = ContainerType{};
+	std::fill(container.begin(), container.end(), std::valarray<double>(size));
+	return container;
+}
 
 class GravityModel {
 public:
 
 
-	std::vector<double> mass;
+	std::valarray<double> mass;
 
-	double G=1.;
+	double G=1;
 	int n_active_bodies;
 
 
-	void calculate_momentum(const StateType &x, StateType &dxdt, const double t) {
+	void calculate_momentum(const ContainerType &q, ContainerType &dpdt) {
 		for(auto i = n_active_bodies; i-->0;) {
-			const auto b_x = x[pos_x][i];
-			const auto b_y = x[pos_y][i];
-			const auto b_z = x[pos_z][i];
-			for(int j = x.size(); j --> 0;) {
-				dxdt[pos_x][j] = x[vel_x][j];
-				dxdt[pos_y][j] = x[vel_y][j];
-				dxdt[pos_z][j] = x[vel_z][j];
-				if(i==j) continue;
-				const auto delta_x = x[pos_x][j] - b_x;
-				const auto delta_z = x[pos_y][j] - b_y;
-				const auto delta_y = x[pos_z][j] - b_z;
+			const auto b_x = q[pos_x][i];
+			const auto b_y = q[pos_y][i];
+			const auto b_z = q[pos_z][i];
+			for(int j = q.size(); j --> i+1;) {
+				const auto delta_x = q[pos_x][j] - b_x;
+				const auto delta_z = q[pos_y][j] - b_y;
+				const auto delta_y = q[pos_z][j] - b_z;
 				const auto r2 = delta_x * delta_x
 					+ delta_y * delta_y
 					+ delta_z * delta_z;
 				const auto f = G*mass[i]*mass[j]/(r2*std::sqrt(r2));
-				dxdt[vel_x][j] -= f*delta_x;
-				dxdt[vel_y][j] -= f*delta_y;
-				dxdt[vel_z][j] -= f*delta_z;
+				dpdt[pos_x][j] -= f*delta_x;
+				dpdt[pos_y][j] -= f*delta_y;
+				dpdt[pos_z][j] -= f*delta_z;
 			}
+			for(int j = i; j --> 0;) {
+				const auto delta_x = q[pos_x][j] - b_x;
+				const auto delta_z = q[pos_y][j] - b_y;
+				const auto delta_y = q[pos_z][j] - b_z;
+				const auto r2 = delta_x * delta_x
+					+ delta_y * delta_y
+					+ delta_z * delta_z;
+				const auto f = G*mass[i]*mass[j]/(r2*std::sqrt(r2));
+				dpdt[pos_x][j] -= f*delta_x;
+				dpdt[pos_y][j] -= f*delta_y;
+				dpdt[pos_z][j] -= f*delta_z;
+			}
+		}
+	}
+
+	void calculate_positions(const ContainerType &p, ContainerType &dqdt) {
+		for(int j = p.size(); j --> 0;) {
+			dqdt[pos_x][j] = p[pos_x][j] / mass[j];
+			dqdt[pos_y][j] = p[pos_y][j] / mass[j];
+			dqdt[pos_z][j] = p[pos_z][j] / mass[j];
 		}
 	}
 };
 
-int main() {
+using point_type = std::valarray<double>;
+struct Body {
+	point_type pos;
+	point_type vel;
+	double mass;
+	Body(point_type pos, point_type vel, double mass): pos(pos), vel(vel), mass(mass){}
+};
+
+struct System {
+	ContainerType q;
+	ContainerType p;
+	std::valarray<double> mass;
+};
+
+System rewrite(const std::vector<Body>& bodies) {
+	auto count = bodies.size();
+	auto s = System{};
+	s.q = get_container(count);
+	s.p = get_container(count);
+	s.mass = std::valarray<double>(count);
+	for(auto j = count; j --> 0;) {
+		const auto& body = bodies[j];
+		for(auto dim=3; dim --> 0;) {
+			s.q[dim][j] = body.pos[dim];
+			s.p[dim][j] = body.vel[dim];
+		}
+		s.mass[j] = body.mass;
+	}
+	return s;
+}
+	
+
+struct positions_functor
+{
+    GravityModel &gm;
+
+    positions_functor(GravityModel &agm) : gm(agm) { }
+
+    void operator()( const ContainerType &p , ContainerType &dqdt ) const
+    {
+			gm.calculate_positions(p, dqdt);
+    }
+};
+
+struct momentum_functor
+{
+    GravityModel &gm;
+
+    momentum_functor(GravityModel &agm) : gm(agm) { }
+
+    void operator()( const ContainerType &q , ContainerType &dpdt ) const
+    {
+			gm.calculate_momentum(q, dpdt);
+    }
+};
+
+struct printing_functor {
+	std::ostream& out;
+	printing_functor(std::ostream& out): out(out){}
+	template<typename State>
+	void operator()(const State& x, double t) const {
+		const auto& q = x.first;
+		const auto& p = x.first;
+		for(auto j=q[0].size(); j --> 0;) {
+			out<<q[pos_x][j]<<" "<<q[pos_y][j]<<" "<<q[pos_z][j]<<' ';
+			out<<p[pos_x][j]<<" "<<p[pos_y][j]<<" "<<p[pos_z][j]<<'\n';
+		}
+		std::cout<<std::flush;
+	}
+};
+
+void predict(std::vector<Body> bodies, double t) {
+	auto s = rewrite(bodies);
 	auto gm = GravityModel{};
-	auto state = StateType{};
-	auto n_bodies=100;
-	std::fill(state.begin(), state.end(), std::vector<double>(n_bodies));
-	gm.n_active_bodies = 3.;
-	gm.mass = std::vector<double>(n_bodies);
+	gm.n_active_bodies = s.p.size();
+	gm.mass = s.mass;
+	using namespace boost::numeric::odeint;
+  using StepperType = symplectic_rkn_sb3a_mclachlan< ContainerType >;
+	const double dt = 100.0;
+	integrate_const(
+					StepperType(),
+					std::make_pair(positions_functor(gm), momentum_functor(gm)),
+					std::make_pair(std::ref(s.q), std::ref(s.p)),
+					0.0, t, dt, printing_functor(std::cout));
+}
+
+int main(int argc, char *argv[]) {
+	auto n_bodies=3;
+	auto planets = std::vector<Body>{};
+	double r = 1.;
+	planets.push_back(Body{{-r,0.,0.}, {0.,-r,0.}, 1.});
+	planets.push_back(Body{{0.,0.,0.}, {0.,0.,0.}, 10.});
+	planets.push_back(Body{{r,0.,0.} , {0.,r,0.} , 1.});
+	predict(planets, 1000.);
 }
